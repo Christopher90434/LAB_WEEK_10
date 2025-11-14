149f855b853ed752df802dad732afb3ef412c3a9
Christopher90434 <christopherbryan.susanto@gmail.com>
2025-11-14 16:29:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lab_week_10
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var total: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        total = 0
+        updateText(total)
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            incrementTotal()
+        }
+    }
+
+    private fun incrementTotal() {
+        total++
+        updateText(total)
+    }
+
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text =
+            getString(R.string.text_total, total)
+    }
+}
